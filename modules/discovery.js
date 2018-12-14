4aa0f8d3d08164ceabe0d93a9e986a8a8647c920
guitarYD <krauser085@gmail.com>
2018-12-14 21:30:59 +0900

    add watson-discovery-setup.js

--- modules/discovery.js
+++ modules/discovery.js
@@ -1,6 +1,17 @@
 // environment variable
-const version = env.process.version_date || '2018-03-05'
+const VERSION = env.process.version_date || '2018-03-05'
+const DEFAULT_NAME = env.process.default_name || 'discoveryTestApp'
 
 // modules
 const DiscoveryV1 = require('watson-developer-cloud/discovery/v1')
-const discovery = new DiscoveryV1({ version })
+const discovery = new DiscoveryV1({ version: VERSION })
+
+// make 'query' a promise function
+discovery.query = Promise.promisify(discovery.query)
+
+const discoverySetup = new WatsonDiscoverySetup(discovery);
+const discoverySetupParams = {
+  default_name: DEFAULT_NAME,
+  config_name: 'food-review-config',   // instead of 'Default Configuration',
+  model_id: model_id
+};
