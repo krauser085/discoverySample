47fdd5b1b5957511c6b6ee622882b8f71e949115
Yongdeok An <an-yongdeok-tev@tdi.co.jp>
2018-12-19 13:25:43 +0900

    add more logs on front-end

--- public/js/views/FormView.js
+++ public/js/views/FormView.js
@@ -1,11 +1,14 @@
 import View from './View.js'
 
+const tag = '[FormView]'
+
 export default function FormView (el) {
   View.call(this, el)
   this.inputEl = el.querySelector('[type=text]')
   this.resetEl = el.querySelector('[type=reset]')
 
   this.init = function () {
+    console.log(tag, 'init()')
     this.showResetBtn(false)
     this.bindEvents()
     return this
